3189517f31baea361be5f8c46c7822a299c4cf79
John Fredrik Bendvold <johnfb@stud.ntnu.no>
2024-06-06 13:48:39 +0200

    Added fonts

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import React, { useCallback, useState, useEffect } from 'react';
+import { Redirect, useRouter } from 'expo-router';
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function Index() {
+  const [fontsLoaded, fontError] = useFonts({ // Load fonts
+    'Nunito': require('../assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
+  });
+  const router = useRouter();
+
+  /**
+   * Called when the root view is laid out
+   */
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  // If the fonts are not loaded
+  if (!fontsLoaded || fontError) {
+    return null;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        This is the index screen. Edit the file app/index.js to change this
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
+    maxWidth: 300,
+    fontFamily: 'Nunito-Bold',
+  },
+});
